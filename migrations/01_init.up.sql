03f960cb4b87599de6414a70e9a02912cb04a633
shivam <shivam@sfninety.com>
2022-05-23 20:59:51 +0100

    mig

--- migrations/01_init.up.sql
+++ migrations/01_init.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users  (
     id UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
     phone_number TEXT NOT NULL,
     verified boolean NOT NULL DEFAULT false,
@@ -6,7 +6,13 @@ CREATE TABLE users (
     device_identifier TEXT,
     password_hash TEXT,
     created TIMESTAMPTZ,
-    updated TIMESTAMPTZ,
-)
+    updated TIMESTAMPTZ
+);
 
-CREATE INDEX ON users (id, phone_number);
+
+CREATE TABLE IF NOT EXISTS verifications (
+    id UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+    phone_number TEXT NOT NULL,
+    otp TEXT NOT NULL,
+    expiry TIMESTAMPTZ
+);
--- migrations/02_verifications.down.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX verifications@verifications_id_idx;
-DROP TABLE IF EXISTS verifications;
