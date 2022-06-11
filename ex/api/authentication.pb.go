3d91e8a4e4e6b055edc19b725af47f7193b43643
Shivam <shivam@sfninety.com>
2022-06-11 17:28:35 +0100

    aks deployment

--- ex/api/authentication.pb.go
+++ ex/api/authentication.pb.go
@@ -248,6 +248,53 @@ func (x *AccessPairResponse) GetRefreshToken() string {
 	return ""
 }
 
+type AuthenticateRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	AccessToken string `protobuf:"bytes,1,opt,name=access_token,json=accessToken,proto3" json:"access_token,omitempty"`
+}
+
+func (x *AuthenticateRequest) Reset() {
+	*x = AuthenticateRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_ex_api_authentication_proto_msgTypes[4]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *AuthenticateRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*AuthenticateRequest) ProtoMessage() {}
+
+func (x *AuthenticateRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_ex_api_authentication_proto_msgTypes[4]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use AuthenticateRequest.ProtoReflect.Descriptor instead.
+func (*AuthenticateRequest) Descriptor() ([]byte, []int) {
+	return file_ex_api_authentication_proto_rawDescGZIP(), []int{4}
+}
+
+func (x *AuthenticateRequest) GetAccessToken() string {
+	if x != nil {
+		return x.AccessToken
+	}
+	return ""
+}
+
 var File_ex_api_authentication_proto protoreflect.FileDescriptor
 
 var file_ex_api_authentication_proto_rawDesc = []byte{
@@ -276,9 +323,13 @@ var file_ex_api_authentication_proto_rawDesc = []byte{
 	0x01, 0x28, 0x09, 0x52, 0x0b, 0x61, 0x63, 0x63, 0x65, 0x73, 0x73, 0x54, 0x6f, 0x6b, 0x65, 0x6e,
 	0x12, 0x23, 0x0a, 0x0d, 0x72, 0x65, 0x66, 0x72, 0x65, 0x73, 0x68, 0x5f, 0x74, 0x6f, 0x6b, 0x65,
 	0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0c, 0x72, 0x65, 0x66, 0x72, 0x65, 0x73, 0x68,
-	0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x42, 0x21, 0x5a, 0x1f, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e,
-	0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x66, 0x6e, 0x69, 0x6e, 0x65, 0x74, 0x79, 0x2f, 0x61, 0x75, 0x74,
-	0x68, 0x2f, 0x65, 0x78, 0x2f, 0x61, 0x70, 0x69, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x22, 0x38, 0x0a, 0x13, 0x41, 0x75, 0x74, 0x68, 0x65, 0x6e, 0x74,
+	0x69, 0x63, 0x61, 0x74, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x21, 0x0a, 0x0c,
+	0x61, 0x63, 0x63, 0x65, 0x73, 0x73, 0x5f, 0x74, 0x6f, 0x6b, 0x65, 0x6e, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x09, 0x52, 0x0b, 0x61, 0x63, 0x63, 0x65, 0x73, 0x73, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x42,
+	0x21, 0x5a, 0x1f, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x66,
+	0x6e, 0x69, 0x6e, 0x65, 0x74, 0x79, 0x2f, 0x61, 0x75, 0x74, 0x68, 0x2f, 0x65, 0x78, 0x2f, 0x61,
+	0x70, 0x69, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -293,12 +344,13 @@ func file_ex_api_authentication_proto_rawDescGZIP() []byte {
 	return file_ex_api_authentication_proto_rawDescData
 }
 
-var file_ex_api_authentication_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
+var file_ex_api_authentication_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
 var file_ex_api_authentication_proto_goTypes = []interface{}{
 	(*PhoneNumberRequest)(nil),  // 0: PhoneNumberRequest
 	(*VerifyOTPRequest)(nil),    // 1: VerifyOTPRequest
 	(*RegistrationRequest)(nil), // 2: RegistrationRequest
 	(*AccessPairResponse)(nil),  // 3: AccessPairResponse
+	(*AuthenticateRequest)(nil), // 4: AuthenticateRequest
 }
 var file_ex_api_authentication_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -362,6 +414,18 @@ func file_ex_api_authentication_proto_init() {
 				return nil
 			}
 		}
+		file_ex_api_authentication_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*AuthenticateRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -369,7 +433,7 @@ func file_ex_api_authentication_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_ex_api_authentication_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   4,
+			NumMessages:   5,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
